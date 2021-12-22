2c6c77293a3ec3bf18b3791d26b74319a8f75fda
Juliana <julianasouzamelo@live.com>
2021-12-22 10:48:36 -0300

    adicionar gif ao agradecer pelo pr

--- src/action.js
+++ src/action.js
@@ -1,4 +1,4 @@
-import fetch from 'node-fetch';
+const fetch = require('node-fetch');
 const core = require('@actions/core');
 const github = require('@actions/github');
 
@@ -10,7 +10,7 @@ async function run() {
 
     const randomPos = Math.round(Math.random() * 50);
     const url = `https://api.tenor.com/v1/search?=thank%20you&pos=${randomPos}&limit=1&media_filter=minimal&contentfilter=high&key=${TENOR_TOKEN}`
-
+    console.log(response)
     const response = await fetch(url);
     const { results } = await response.json();
     const gifUrl = results[0].media[0].tinygif.url;
