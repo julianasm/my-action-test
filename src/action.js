55ad2e0cc9ff1e52364164e995597b12bcef3f59
Juliana <julianasouzamelo@live.com>
2021-12-22 11:20:45 -0300

    adicionar gif ao agradecer pelo pr

--- src/action.js
+++ src/action.js
@@ -1,6 +1,7 @@
 const fetch = require('node-fetch');
 const core = require('@actions/core');
 const github = require('@actions/github');
+const { default: axios } = require('axios');
 
 
 
@@ -8,14 +9,16 @@ async function run() {
     const GITHUB_TOKEN = core.getInput('GITHUB_TOKEN');
     const TENOR_TOKEN = core.getInput('TENOR_TOKEN');
 
-    const randomPos = Math.round(Math.random() * 50);
+    const randomPos = Math.floor(Math.random() * 50);
     const url = `https://api.tenor.com/v1/search?=thank%20you&pos=${randomPos}&limit=1&media_filter=minimal&contentfilter=high&key=${TENOR_TOKEN}`
-    
-    const response = await fetch(url);
-    console.log(response)
-    const { results } = await response.json();
-    const gifUrl = results[0].media[0].tinygif.url;
 
+    await axios.get(url).then(function (response) {
+        let request_data = response.data.results;
+        gifUrl = request_data[0].media[0].tinygif.url;
+    }).catch(function (error) {
+        console.log(error)
+    })
+    
     const octokit = github.getOctokit(GITHUB_TOKEN);
 
     const { context = {} } = github;
